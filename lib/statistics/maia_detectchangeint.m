19b9d826861e13ec4d3af661ca16518a472fda2e
ntnumartin <martin.heyn@ntnu.no>
2017-08-25 15:46:35 +0200

    GLRT

--- lib/statistics/maia_detectchangeint.m
+++ lib/statistics/maia_detectchangeint.m
@@ -1,7 +1,7 @@
-function[g] = maia_detectchangeint(data,pd_t,windowsize)
- 
-    [mu0,S0,nu0] = fitt(data);
-    M = length(data);
+function[g_nu,g_S,g_Snu] = maia_detectchangeint(data,pd_t,windowsize)
+
+    cutdata = data(370:1150,:);
+    [mu0,S0,nu0] = fitt(cutdata);
     
     for k = windowsize+1:1:length(data)
         sample = data(k-windowsize:k,:);
@@ -9,10 +9,18 @@ function[g] = maia_detectchangeint(data,pd_t,windowsize)
         S = pd_t(k,2);
         nu = pd_t(k,3);
         
-        datasum0 = sum(log(1+(1/(nu0*S0)*(sample-mu0).^2)));
-        datasum = sum(log(1+(1/(nu*S0)*(sample-mu0).^2)));
+        datasum0_nu = sum(log(1+(1/(nu0*S0)*(sample-mu0).^2)));
+        datasum1_nu = sum(log(1+(1/(nu*S0)*(sample-mu0).^2)));
         
-        g(k) = -((2+nu)/2)*datasum + ((2+nu0)/2)*datasum0;       
+        datasum0_S = sum(log(1+(1/(nu0*S0))*(sample-mu0).^2));
+        datasum1_S = sum(log(1+(1/(nu0*S)*(sample-mu0).^2)));
+        
+        datasum0_Snu = sum(log(1+(1/(nu0*S0))*(sample-mu0).^2));
+        datasum1_Snu = sum(log(1+(1/(nu*S)*(sample-mu0).^2)));
+        
+        g_nu(k) = - ((2+nu)/2)*datasum1_nu + ((2+nu0)/2)*datasum0_nu;
+        g_S(k) =  - ((2+nu0)/2)*datasum1_S + ((2+nu0)/2)*datasum0_S;
+        g_Snu(k) = - ((2+nu)/2)*datasum1_Snu + ((2+nu0)/2)*datasum0_Snu;
         
     end
 
---
+++ lib/statistics/maia_detectchangeintbi.m
@@ -0,0 +1,35 @@
+function[g_nu,g_S,g_Snu] = maia_detectchangeintbi(data,S_vec,nu_vec,windowsize)
+ 
+    %[mu0,S0,nu0] = (data(370:1150),:);
+    
+    cutdata = data(370:1150,:);
+    [mu0,S0,nu0] = fitt(cutdata);
+    
+    M = length(data);
+    S = zeros(2,2);
+    
+    for k = windowsize+1:1:length(data)
+        sample = data(k-windowsize:k,:);
+        S(:,:) = S_vec(k,:,:);
+        nu = nu_vec(k);
+        
+        for m = 1:1:length(sample)
+            datamulti0(m) = (sample(m,:) - mu0)*S0*(sample(m,:)-mu0)';
+            datamulti1(m) = (sample(m,:) - mu0)*S*(sample(m,:)-mu0)';
+        end
+
+        datasum0_nu = sum(log(1+((1/(nu0))*datamulti0)));
+        datasum1_nu = sum(log(1+((1/(nu))*datamulti0)));
+        
+        datasum0_S = sum(log(1+((1/(nu0))*datamulti0)));
+        datasum1_S = sum(log(1+((1/(nu0))*datamulti1)));
+        
+        datasum0_Snu = sum(log(1+((1/(nu0))*datamulti0)));
+        datasum1_Snu = sum(log(1+((1/(nu))*datamulti1)));
+        
+        g_nu(k) = (-((2+nu)/2)*datasum1_nu + ((2+nu0)/2)*datasum0_nu);
+        g_S(k) = (-((2+nu0)/2)*datasum1_S + ((2+nu0)/2)*datasum0_S);
+        g_Snu(k) = (-((2+nu)/2)*datasum1_Snu + ((2+nu0)/2)*datasum0_Snu);
+    end
+
+end
